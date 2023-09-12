2121c815def35bf02857bee887c162e5795d9efe
Glenn <Glenn@test.com>
2023-09-12 08:46:57 +0800

    mysql promisewrapper for async

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,22 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: "localhost",
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "kanban",
 });
 
-module.exports = connection;
+const dbQuery = (sql, queryArr) => {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, queryArr, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+module.exports = { connection: connection, dbQuery: dbQuery };
